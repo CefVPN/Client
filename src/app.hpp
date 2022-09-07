c339d54ce945feb85f6ad4a31821acaed22ae097
OPstriker <76495154+OPstriker@users.noreply.github.com>
2022-09-06 20:18:33 -0400

    Initial Commit

---
+++ src/app.hpp
@@ -0,0 +1,20 @@
+#include <include/cef_app.h>
+
+// Implement application-level callbacks for the browser process.
+class SimpleApp : public CefApp, public CefBrowserProcessHandler {
+ public:
+  SimpleApp();
+
+  // CefApp methods:
+  CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() override {
+    return this;
+  }
+
+  // CefBrowserProcessHandler methods:
+  void OnContextInitialized() override;
+  CefRefPtr<CefClient> GetDefaultClient() override;
+
+ private:
+  // Include the default reference counting implementation.
+  IMPLEMENT_REFCOUNTING(SimpleApp);
+};
