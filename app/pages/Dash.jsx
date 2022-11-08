af44b17673d6c3fc11d649d7be70174128bdd13d
OPstriker <76495154+OPstriker@users.noreply.github.com>
2022-11-07 21:19:52 -0500

    UI Update!
    - Working On a Dashboard...

--- app/pages/Dash.jsx
+++ app/pages/Dash.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
-import { Button } from '@mui/material'
+import { Button, Box } from '@mui/material'
 import { Handler, Disconnect, isFullscreen } from '../components/Handler'
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
 export default function Dash() {
   return (
     <div className="text-white absolute ml-20">
-        <div className="mt-6 ml-20 flex space-x-2">
-            <Button variant='outlined' onClick={Handler} className="mt-20">Connect</Button>
-            <Button variant='outlined' onClick={Disconnect} className="mt-20">Disconnect</Button> 
-            <Button variant='outlined' onClick={isFullscreen} className="mt-20">Check Fullscreen</Button>
+        <div className="main ml-9 mt-8">
+          <Box
+          sx={{
+            width: 300,
+            height: 300,
+            backgroundColor: "#282C34",
+            borderRadius: "12px",
+            boxShadow: 3,
+            ":hover": {
+              boxShadow: 5,
+              transition: "box-shadow",
+              transitionDuration: "4000"
+            }
+          }}>
+            <h1 className="text-lg pt-5 pl-5 opacity-75">Virtual Location</h1>
+            <ComposableMap className='pr-5 pt-12'>
+              <Geographies geography={"/features.json"}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography key={geo.rsmKey} geography={geo} fill="#4f525c" stroke='none' />
+                ))
+              }
+              </Geographies>
+            </ComposableMap>
+          </Box>
         </div>
     </div>
   )
