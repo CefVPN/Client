c453d98277e98efc0ba2cac07d9773c37d32a63c
OP <76495154+OPstriker@users.noreply.github.com>
2022-11-15 22:38:28 -0500

    OpenVPN Update!
    - Updated OpenVPN3 to the Latest commit.
    - Adding ovpn-dco-win support.

--- app/pages/Dash.jsx
+++ app/pages/Dash.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { Button, Box } from '@mui/material'
 import { Handler, Disconnect, isFullscreen } from '../components/Handler'
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import { ComposableMap, Geographies, Geography, Marker } from "react-simple-maps";
 
 export default function Dash() {
+
+  let loc;
+
+  fetch('https://ipinfo.io?token=a416934c6bf2af')
+    .then(res => res.json())
+    .then(data => {
+        let output = parseFloat(data.loc);
+        loc = output;
+
+         console.log((loc));
+    })
+
   return (
     <div className="text-white absolute ml-20">
         <div className="main ml-9 mt-8">
@@ -21,16 +33,22 @@ export default function Dash() {
             }
           }}>
             <h1 className="text-lg pt-5 pl-5 opacity-75">Virtual Location</h1>
-            <ComposableMap className='pr-5 pt-12'>
+            <ComposableMap className="flex">
               <Geographies geography={"/features.json"}>
               {({ geographies }) =>
                 geographies.map((geo) => (
-                  <Geography key={geo.rsmKey} geography={geo} fill="#4f525c" stroke='none' />
+                  <Geography key={geo.rsmKey} geography={geo} fill="#4f525c" stroke='#4f525c' pointerEvents={"none"} />
                 ))
               }
               </Geographies>
+              <Marker coordinates={[-112.0740, 33.4484]}>
+                <circle r={8} fill="#F53" />
+              </Marker>
             </ComposableMap>
           </Box>
+            <Button variant='outlined' onClick={Handler} className="mt-20">Connect</Button>
+            <Button variant='outlined' onClick={Disconnect} className="mt-20">Disconnect</Button> 
+            <Button variant='outlined' onClick={isFullscreen} className="mt-20">Check Fullscreen</Button>
         </div>
     </div>
   )
